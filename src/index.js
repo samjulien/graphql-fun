18cb023bba8982a3aa967d18f115b25b5f742599
Sam Julien <spjulien@gmail.com>
2020-02-05 19:38:20 -0800

    Add authentication

--- src/index.js
+++ src/index.js
@@ -26,7 +26,8 @@ const server = new ApolloServer({
   },
   context: async () => ({
     models,
-    me: await models.User.findByLogin("rwieruch")
+    me: await models.User.findByLogin("rwieruch"),
+    secret: process.env.SECRET
   })
 });
 
@@ -48,6 +49,8 @@ const createUsersWithMessages = async () => {
   await models.User.create(
     {
       username: "rwieruch",
+      email: "hello@robin.com",
+      password: "rwieruch",
       messages: [
         {
           text: "Published the Road to learn React"
@@ -61,6 +64,8 @@ const createUsersWithMessages = async () => {
   await models.User.create(
     {
       username: "ddavids",
+      email: "hello@david.com",
+      password: "ddavids",
       messages: [
         {
           text: "Happy to release ..."
