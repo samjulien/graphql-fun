9e8b335c67fbddc3b3c8be612839f317bab7933f
Sam Julien <spjulien@gmail.com>
2020-02-06 09:28:06 -0800

    Add graphql resolvers

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import "dotenv/config";
 import cors from "cors";
 import express from "express";
-import { ApolloServer } from "apollo-server-express";
+import jwt from "jsonwebtoken";
+import { ApolloServer, AuthenticationError } from "apollo-server-express";
 import schema from "./schema";
 import resolvers from "./resolvers";
 import models, { sequelize } from "./models";
@@ -9,6 +10,17 @@ import models, { sequelize } from "./models";
 const app = express();
 
 app.use(cors());
+
+const getMe = async req => {
+  const token = req.headers["x-token"];
+  if (token) {
+    try {
+      return await jwt.verify(token, process.env.SECRET);
+    } catch (e) {
+      throw new AuthenticationError("Your session expired. Sign in again.");
+    }
+  }
+};
 
 const server = new ApolloServer({
   typeDefs: schema,
@@ -24,11 +36,14 @@ const server = new ApolloServer({
       message
     };
   },
-  context: async () => ({
-    models,
-    me: await models.User.findByLogin("rwieruch"),
-    secret: process.env.SECRET
-  })
+  context: async ({ req }) => {
+    const me = await getMe(req);
+    return {
+      models,
+      me,
+      secret: process.env.SECRET
+    };
+  }
 });
 
 server.applyMiddleware({ app, path: "/graphql" });
